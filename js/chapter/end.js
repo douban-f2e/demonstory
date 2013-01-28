e9096842afba81ee7ee5f6ad91582736c54f27ff
kejun <kejun@kejuns-mac.local>
2013-01-28 17:16:59 +0800

    片尾部分

---
+++ js/chapter/end.js
@@ -0,0 +1,32 @@
+
+define([
+    'mo/lang',
+    'dollar',
+    'eventmaster',
+    'movie/util',
+    'movie/demon'
+], function(_, $, event, util, demon){
+
+    var wait = util.wait,
+
+        DESC = "无趣的生活的从那一次点击开始改变...", 
+
+        jump_count = 0;
+
+    return {
+
+        announce: function(screen){
+            return screen('豆瓣改变生活', DESC, 1000);
+        },
+
+        main: function(win, promise){
+        },
+
+        reset: function(){
+        }
+
+    };
+
+    
+
+});
