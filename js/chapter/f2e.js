3935e1d185bafd2f0d3cfff9f12a2de9d85d384c
hxgdzyuyi <hxgdzyuyi@gmail.com>
2013-01-29 09:53:44 +0800

    Tidy js

--- js/chapter/f2e.js
+++ js/chapter/f2e.js
@@ -59,31 +59,20 @@ define([
             }
 
             f2es = initF2es()
-            var _time = 0
-            var w = function(time){
-                time = time*1000
-                _time += time
-                return _time
-            }
-            var t = function(time){
-                time = time*1000
-                _time += time
-                return time
-            }
 
             wrapper = $('.wrapper', win.document)
             var baseLeft = wrapper[0].offsetLeft
 
             var kejun = f2es.kejun.demon
               , yy = f2es.yy.demon;
-            wait(w(0.4)).done(function(){
-                return kejun.walk([ baseLeft, 200 ], t(3));
+            wait(400).done(function(){
+                return kejun.walk([ baseLeft, 200 ], 3000);
             }).follow().done(function(){
-                return kejun.speak('要说前端开发是什么， 那得花开两朵，各自一枝', t(5), 3)
+                return kejun.speak('要说前端开发是什么， 那得花开两朵，各自一枝', 5000, 3)
             }).follow().done(function(){
-                return wait(w(0.3))
+                return wait(300)
             }).follow().done(function(){
-                yy.speak('要说前端开发是什么， 那得花开两朵，各自一枝', t(5), 3)
+                yy.speak('要说前端开发是什么， 那得花开两朵，各自一枝', 5000, 3)
             })
             return promise;
         },
