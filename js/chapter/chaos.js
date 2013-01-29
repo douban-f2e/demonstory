5f8b8a10fba2cb548eaff9076d2d41e05e5320cd
ktmud <jyyjcc@gmail.com>
2013-01-29 10:13:44 +0800

    move Screen -> Camera, as a standalone module

--- js/chapter/chaos.js
+++ js/chapter/chaos.js
@@ -4,79 +4,14 @@ define([
   'choreo',
   'eventmaster',
   'movie/util',
+  'movie/camera',
   'movie/demon'
-], function(_, $, choreo, event, util, demon){
+], function(_, $, choreo, event, util, camera, demon){
 
   var wait = util.wait,
   jump_count = 0;
 
   var action = choreo().play();
-
-  function Screen(win) {
-    this.action = choreo().play();
-    this.win = win;
-    this.doc = $(win.document.body);
-  }
-
-  Screen.prototype.pan = function(args) {
-    var cpoint = args.cpoint;
-    var elem = args.elem;
-    var offset = args.offset;
-    var zoom = args.zoom || 1.5;
-    var duration = args.duration || 1200;
-
-    var action = choreo().play();
-
-    var win = this.win;
-    var doc = this.doc;
-
-    var x = 0;
-    var y = 0;
-    if (cpoint) {
-      x = cpoint[0];
-      y = cpoint[1];
-    } else {
-      var o = $(elem, win).offset();
-      if (o) {
-        x = o.left + o.width / 2;
-        y = o.top + o.height / 2;
-      }
-    }
-
-    var l = doc.width() / 2;
-    var t = doc.height() / 2;
-
-    x = parseInt((win.innerWidth / 2 - x) * zoom);
-    y = parseInt(t * zoom - t + (win.innerHeight / 2 - y * zoom));
-
-    if (offset) {
-      x += (offset.left || 0);
-      y += (offset.top || 0);
-    }
-
-    var translate = 'translate(' + x + 'px,' + y + 'px)';
-    action.actor(doc[0], {
-      transform: translate
-    }, duration);
-    return action.actor(doc[0], {
-      transform: 'scale(' + zoom + ',' + zoom + ')'
-    }, duration).follow();
-  };
-  Screen.prototype.reset = function(duration) {
-    duration = duration || 320;
-    var action = choreo().play();
-
-    var doc = this.doc;
-
-    action.actor(doc[0], {
-      transform: 'scale(1,1)' 
-    }, duration);
-
-    return action.actor(doc[0], {
-      transform: 'translate(0,0)' 
-    }, duration).follow();
-  };
-
 
   return {
 
@@ -90,7 +25,7 @@ define([
 
       var doc = win.document;
 
-      var screen = new Screen(win);
+      var screen = camera(win);
 
       //var top_nav = $('.top-nav', doc);
       var header = $('#header_wrapper', doc);
